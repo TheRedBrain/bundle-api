f1824d82dbb5955e1fd80e22d0d6e7ba2ea36fae
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-09-15 00:01:26 +0200

    Working NeoForge module

--- common/src/main/java/com/github/theredbrain/bundleapi/BundleAPI.java
+++ common/src/main/java/com/github/theredbrain/bundleapi/BundleAPI.java
@@ -2,14 +2,11 @@ package com.github.theredbrain.bundleapi;
 
 import com.github.theredbrain.bundleapi.component.type.CustomBundleContentsComponent;
 import com.github.theredbrain.bundleapi.predicate.item.CustomBundleContentsPredicate;
-import net.fabricmc.api.ModInitializer;
 import net.minecraft.component.ComponentType;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.ContainerComponentModifier;
 import net.minecraft.loot.ContainerComponentModifiers;
 import net.minecraft.predicate.item.ItemSubPredicate;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,26 +17,15 @@ public class BundleAPI {
 	public static final String MOD_ID = "bundleapi";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	public static ComponentType<CustomBundleContentsComponent> CUSTOM_BUNDLE_CONTENTS_COMPONENT;
-	public static ItemSubPredicate.Type<CustomBundleContentsPredicate> CUSTOM_BUNDLE_CONTENTS_ITEM_SUB_PREDICATE;
+	public static ComponentType<CustomBundleContentsComponent> CUSTOM_BUNDLE_CONTENTS_COMPONENT = ComponentType.<CustomBundleContentsComponent>builder().codec(CustomBundleContentsComponent.CODEC).packetCodec(CustomBundleContentsComponent.PACKET_CODEC).cache().build();
+	public static ItemSubPredicate.Type<CustomBundleContentsPredicate> CUSTOM_BUNDLE_CONTENTS_ITEM_SUB_PREDICATE = new ItemSubPredicate.Type<>(CustomBundleContentsPredicate.CODEC);
 	public static ContainerComponentModifier<CustomBundleContentsComponent> CUSTOM_BUNDLE_CONTENTS_CONTAINER_COMPONENT_MODIFIER;
 
 	public static void init() {
 		LOGGER.info("Customized Bundles!");
 	}
 
-	static { // !!
-		CUSTOM_BUNDLE_CONTENTS_COMPONENT = Registry.register(
-				Registries.DATA_COMPONENT_TYPE,
-				identifier("custom_bundle_contents"),
-				ComponentType.<CustomBundleContentsComponent>builder().codec(CustomBundleContentsComponent.CODEC).packetCodec(CustomBundleContentsComponent.PACKET_CODEC).cache().build()
-		);
-		CUSTOM_BUNDLE_CONTENTS_ITEM_SUB_PREDICATE = Registry.register(
-				Registries.ITEM_SUB_PREDICATE_TYPE,
-				"custom_bundle_contents",
-				new ItemSubPredicate.Type<>(CustomBundleContentsPredicate.CODEC)
-		);
-
+	static {
 		CUSTOM_BUNDLE_CONTENTS_CONTAINER_COMPONENT_MODIFIER = new ContainerComponentModifier<CustomBundleContentsComponent>() {
 			@Override
 			public ComponentType<CustomBundleContentsComponent> getComponentType() {
